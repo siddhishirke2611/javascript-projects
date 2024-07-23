5f8c491f89f2174cd8b0ba77f22ade7c40c2698c
siddhi <shirkesiddhi2611@gmail.com>
2024-07-23 21:16:53 +0530

    to do list

---
+++ ToDoList/index.js
@@ -0,0 +1,12 @@
+document.querySelector('#push').onClick = function () {
+    if
+        (document.querySelector('#newtask input').value.length == 0) {
+            console.log('siddhi here');
+        alert("please enter a task")
+        
+    }
+    else{
+        (document.querySelector('#newtask input').value.length == 0)
+        alert("please enter a task2")
+    }
+}
