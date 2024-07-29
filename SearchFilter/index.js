84af3be764c93d963ed951fa9ecda980a16057f0
siddhi <siddhishirke.hkb@gmail.com>
2024-07-29 15:31:42 +0530

    to do list changes

---
+++ SearchFilter/index.js
@@ -0,0 +1,52 @@
+let products = {
+    data: [
+      {
+        productName: "Regular White T-Shirt",
+        category: "Topwear",
+        price: "30",
+        image: "white-tshirt.jpg",
+      },
+      {
+        productName: "Beige Short Skirt",
+        category: "Bottomwear",
+        price: "49",
+        image: "short-skirt.jpg",
+      },
+      {
+        productName: "Sporty SmartWatch",
+        category: "Watch",
+        price: "99",
+        image: "sporty-smartwatch.jpg",
+      },
+      {
+        productName: "Basic Knitted Top",
+        category: "Topwear",
+        price: "29",
+        image: "knitted-top.jpg",
+      },
+      {
+        productName: "Black Leather Jacket",
+        category: "Jacket",
+        price: "129",
+        image: "black-leather-jacket.jpg",
+      },
+      {
+        productName: "Stylish Pink Trousers",
+        category: "Bottomwear",
+        price: "89",
+        image: "pink-trousers.jpg",
+      },
+      {
+        productName: "Brown Men's Jacket",
+        category: "Jacket",
+        price: "189",
+        image: "brown-jacket.jpg",
+      },
+      {
+        productName: "Comfy Gray Pants",
+        category: "Bottomwear",
+        price: "49",
+        image: "comfy-gray-pants.jpg",
+      },
+    ],
+  };
