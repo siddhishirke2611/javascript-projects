485835a615eecde9b3c73d181233b9313c7538a9
siddhi <shirkesiddhi2611@gmail.com>
2024-09-10 20:34:04 +0530

    function

---
+++ functions.js
@@ -0,0 +1,13 @@
+// function is block of reusable code
+// function sum(a , b){
+//    return a + b;
+    
+// }
+// console.log(sum(5,5));
+
+
+//function invocation
+function greet(name){
+    console.log("hello " + name + ", welcome");
+}
+greet("siddhi")
