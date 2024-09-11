4a78fa844495c82bee9bb00224d3afb48c586bdc
siddhi <siddhishirke.hkb@gmail.com>
2024-09-11 17:50:24 +0530

    array

---
+++ array.js
@@ -0,0 +1,14 @@
+let fruits = new Array("apple", "orange", "banana");
+console.log(fruits[0]);
+
+//for each method
+
+fruits.forEach((curElem, index, arr) =>{
+    console.log(`${curElem} ${index}`);
+})
+
+//map function 
+
+fruits.map(() =>{
+    
+})
